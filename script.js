1f43ec67b9154ce254eb56e9f51ff84656446d32
Asad <asadullahkhan0098@gmail.com>
2024-07-10 14:33:46 +0530

    Add ticTacToe object

--- script.js
+++ script.js
@@ -1,25 +1,92 @@
 const gameboard = (function() {
-    const arr = new Array(9);
+    let arr = [null, null, null, null, null, null, null, null, null];
     const setArr = (idx, val) => {
         arr[idx] = val;
     };
     const getArr = () => arr;
-    return {setArr, getArr};
+    const resetArr = () => {
+        arr = arr.fill(null);
+    };
+    return {setArr, getArr, resetArr};
 })();
 
 function createPlayer(name, choice) {
     const playerName = name;
     const playerChoice = choice;
-    function drawOnBoard(cell) {
-        gameboard.setArr(cell, this.playerChoice)
+    function drawOnBoard() {
+        let cell = prompt('Which cell?');
+        while(gameboard.getArr()[+cell] !== null) {
+            cell = prompt('Already filled, try another cell');    
+        }
+        gameboard.setArr(cell, this.playerChoice);
     }
     return {playerName, playerChoice, drawOnBoard};
 }
 
-const com = (function(choice) {
+function createCom(choice) {
     const {playerName, playerChoice} = createPlayer('Com', choice);
+    const getRandomCell = () => {
+        const indexesOfUnfilledCells = [];
+        for(let i = 0; i < 9; i++) {
+            if(gameboard.getArr()[i] === null) indexesOfUnfilledCells.push(i);
+        }
+        const randomIndexOfUnfilledCells = Math.floor(Math.random() * indexesOfUnfilledCells.length);
+        return indexesOfUnfilledCells[randomIndexOfUnfilledCells];
+        
+    }
     function drawOnBoard() {
+        let randomCell = getRandomCell();
+        gameboard.setArr(randomCell, this.playerChoice);
+    }
+    return {playerName, playerChoice, drawOnBoard};
+};
 
+const ticTacToe = (function() {
+    const player1 = createPlayer(prompt('Enter your name'), prompt('X or O'));
+    const comChoice = player1.playerChoice === 'x' ? 'o' : 'x';
+    const com = createCom(comChoice);
+    const getResult = () => {
+        const gameboardArr = gameboard.getArr();
+        for(let i = 0; i < 9; i+=3) {
+            if((gameboardArr[i] && gameboardArr[i+1] && gameboardArr[i+2]) && gameboardArr[i] === gameboardArr[i+1] && gameboardArr[i+1] === gameboardArr[i+2]) {
+                return `${gameboardArr[i]} has won!`;
+            }
+        }
+        for(let i = 0; i < 3; i++) {
+            if((gameboardArr[i] && gameboardArr[i+3] && gameboardArr[i+6]) && gameboardArr[i] === gameboardArr[i+3] && gameboardArr[i+3] === gameboardArr[i+6]) {
+                return `${gameboardArr[i]} has won!`;
+            }
+        }
+        if((gameboardArr[0] && gameboardArr[4] && gameboardArr[8]) && gameboardArr[0] === gameboardArr[4] && gameboardArr[4] === gameboardArr[8]) {
+            return `${gameboardArr[0]} has won!`;
+        }
+        else if((gameboardArr[2] && gameboardArr[4] && gameboardArr[6]) && gameboardArr[2] === gameboardArr[4] && gameboardArr[4] === gameboardArr[6]) {
+            return`${gameboardArr[2]} has won!`;
+        }
+        else if(gameboardArr.filter(el => el === 'x' || el === 'o').length === 9) return "It's a draw!";
+
+        return 'No result yet!';
     }
 
-})
+    function startGame() {
+        while(true) {
+            if(player1.playerChoice === 'x') {
+                player1.drawOnBoard();
+                if(getResult() !== 'No result yet!') {
+                break;
+            }
+                com.drawOnBoard();
+            }
+            else {
+                com.drawOnBoard();
+                if(getResult() !== 'No result yet!') {
+                break;
+            }
+                player1.drawOnBoard();
+            }
+        }
+        return getResult();
+    }
+    const resetGame = () => gameboard.resetArr();
+    return {startGame, resetGame};
+    })();
